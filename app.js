4e66c4d3c613e4a205d697201f4b8d02c61131f6
kyle aban <cooldude859@gmail.com>
2024-08-15 13:28:37 -0400

    First commit

---
+++ app.js
@@ -0,0 +1,32 @@
+
+require('dotenv').config({ path: `${process.cwd()}/.env` });
+const express = require('express');
+
+const authRouter = require('./route/authRoute');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.status(200).json({
+        status: 'success',
+        message: 'Woohoo! REST is working'
+    });
+});
+
+
+// all routes will be here
+
+app.use('/api/v1/auth', authRouter);
+
+app.use('*', (req, res, next) => {
+    res.status(404).json({
+        status: 'fail',
+        message: 'Route not found'
+    });
+})
+
+const PORT = process.env.APP_PORT || 4000;
+
+app.listen(PORT, () => {
+    console.log('Server up and running', PORT);
+});
